0eb34b9433a026d49bda03d0c6699a4e96be5b5d
syahli kurniawan <syahlikurniawan332@gmail.com>
2023-11-29 01:11:30 +0700

    part 2 clear

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,5 +1,7 @@
 import 'package:calendar_appbar/calendar_appbar.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:ti3c_k4_ppm/pages/category_page.dart';
 import 'package:ti3c_k4_ppm/pages/home_page.dart';
 
 class MainPage extends StatefulWidget {
@@ -10,34 +12,67 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  final List<Widget> _children = [HomePage(), CategoryPage()];
+  int currentIndex = 0;
+
+  void onTapTapped(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //tanggal dan waktu
-      appBar: CalendarAppBar(
-        accent: const Color.fromARGB(255, 48, 206, 53),
-        backButton: false,
-        locale: 'id',
-        onDateChanged: (value) => print(value),
-        firstDate: DateTime.now().subtract(Duration(days: 365)),
-        lastDate: DateTime.now(),
-      ),
-      //bagian paling bawah
-      floatingActionButton: FloatingActionButton(
-        onPressed: (){}, 
-        backgroundColor: Colors.green, 
-        child: Icon(Icons.add),
+        //tanggal dan waktu
+        appBar: (currentIndex == 0)
+            ? CalendarAppBar(
+                accent: const Color.fromARGB(255, 48, 206, 53),
+                backButton: false,
+                locale: 'id',
+                onDateChanged: (value) => print(value),
+                firstDate: DateTime.now().subtract(Duration(days: 365)),
+                lastDate: DateTime.now(),
+              )
+            : PreferredSize(
+                child: Container(
+                    child: Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 36,
+                    horizontal: 16,
+                  ),
+                  child: Text('Categories',
+                      style: GoogleFonts.montserrat(fontSize: 20)),
+                )),
+                preferredSize: Size.fromHeight(100)),
+        //bagian paling bawah
+        floatingActionButton: Visibility(
+          visible: (currentIndex == 0) ? true : false,
+          child: FloatingActionButton(
+            onPressed: () {},
+            backgroundColor: Colors.green,
+            child: Icon(Icons.add),
+          ),
         ),
-        body: HomePage(),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked, bottomNavigationBar: BottomAppBar(
-          child: 
-          Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-            IconButton(onPressed: () {}, icon: Icon(Icons.home)),
+        body: _children[currentIndex],
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        bottomNavigationBar: BottomAppBar(
+          child:
+              Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+            IconButton(
+                onPressed: () {
+                  onTapTapped(0);
+                },
+                icon: Icon(Icons.home)),
             SizedBox(
               width: 20,
             ),
-            IconButton(onPressed: () {}, icon: Icon(Icons.list))
-            ]),
-            ));
+            IconButton(
+                onPressed: () {
+                  onTapTapped(1);
+                },
+                icon: Icon(Icons.list))
+          ]),
+        ));
   }
 }

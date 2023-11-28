ed977eb541a5d494edc87567ca4c216fa3c944ce
syahli kurniawan <syahlikurniawan332@gmail.com>
2023-11-28 14:29:04 +0700

    membuat home dan category

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,3 +1,4 @@
+import 'package:calendar_appbar/calendar_appbar.dart';
 import 'package:flutter/material.dart';
 
 class MainPage extends StatefulWidget {
@@ -11,11 +12,30 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //tanggal dan waktu
+      appBar: CalendarAppBar(
+        accent: const Color.fromARGB(255, 48, 206, 53),
+        backButton: false,
+        locale: 'id',
+        onDateChanged: (value) => print(value),
+        firstDate: DateTime.now().subtract(Duration(days: 365)),
+        lastDate: DateTime.now(),
+      ),
+      //bagian paling bawah
       floatingActionButton: FloatingActionButton(
         onPressed: (){}, 
         backgroundColor: Colors.green, 
         child: Icon(Icons.add),
         ),
-    );
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked, bottomNavigationBar: BottomAppBar(
+          child: 
+          Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+            IconButton(onPressed: () {}, icon: Icon(Icons.home)),
+            SizedBox(
+              width: 20,
+            ),
+            IconButton(onPressed: () {}, icon: Icon(Icons.list))
+            ]),
+            ));
   }
 }

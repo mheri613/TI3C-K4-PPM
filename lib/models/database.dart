413ee5d706c4e2bcab2cf237535780165d0850be
vermi <112502340+mheri613@users.noreply.github.com>
2023-12-03 03:54:25 +0700

    menambahkan model dan menginstall library drift

---
+++ lib/models/database.dart
@@ -0,0 +1,52 @@
+import 'dart:io';
+
+import 'package:drift/drift.dart';
+// These imports are only needed to open the database
+import 'package:drift/native.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as p;
+import 'package:ti3c_k4_ppm/models/category.dart';
+import 'package:ti3c_k4_ppm/models/transaction.dart';
+
+part 'database.g.dart';
+
+@DriftDatabase(
+  // relative import for the drift file. Drift also supports `package:`
+  // imports
+  tables: [Categories, Transactions],
+)
+class AppDb extends _$AppDb {
+  AppDb() : super(_openConnection());
+
+  @override
+  int get schemaVersion => 6;
+
+  // CRUD Category
+  Future<List<Category>> getAllCategoryRepo(int type) async {
+    return await (select(categories)..where((tbl) => tbl.type.equals(type)))
+        .get();
+  }
+
+  Future updateCategoryRepo(int id, String newName) async {
+    return (update(categories)..where((t) => t.id.equals(id))).write(
+      CategoriesCompanion(
+        name: Value(newName),
+      ),
+    );
+  }
+
+  Future deleteCategoryRepo(int id) async {
+    return (delete(categories)..where((t) => t.id.equals(id))).go();
+  }
+}
+
+LazyDatabase _openConnection() {
+  // the LazyDatabase util lets us find the right location for the file async.
+  return LazyDatabase(() async {
+    // put the database file, called db.sqlite here, into the documents folder
+    // for your app.
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(p.join(dbFolder.path, 'db.sqlite'));
+    return NativeDatabase(file);
+  });
+}
